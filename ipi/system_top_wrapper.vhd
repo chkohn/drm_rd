e905ca748956c68d00a0453e2e255e004b778e08
Chris Kohn <ckohn@xilinx.com>
2013-05-02 14:59:20 -0700

    ipi: add top level wrapper vhdl file
    manually added ibufgds and bufg primitives for differential clock input

---
+++ ipi/system_top_wrapper.vhd
@@ -0,0 +1,125 @@
+
+-- lib IP_Integrator_Lib
+library IEEE; use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM; use UNISIM.VCOMPONENTS.ALL; 
+entity system_top_wrapper is
+  port (
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    hdmi_clk : out STD_LOGIC;
+    hdmi_data : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    hdmi_de : out STD_LOGIC;
+    hdmi_hsync : out STD_LOGIC;
+    hdmi_int : in STD_LOGIC;
+    hdmi_vsync : out STD_LOGIC;
+    video_clk_p : in STD_LOGIC;
+    video_clk_n : in STD_LOGIC
+  );
+end system_top_wrapper;
+
+architecture STRUCTURE of system_top_wrapper is
+  component system_top is
+  port (
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    hdmi_clk : out STD_LOGIC;
+    hdmi_data : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    hdmi_hsync : out STD_LOGIC;
+    hdmi_vsync : out STD_LOGIC;
+    hdmi_de : out STD_LOGIC;
+    hdmi_int : in STD_LOGIC;
+    video_clk : in STD_LOGIC
+  );
+  end component system_top;
+  
+  signal video_clk_i : STD_LOGIC;
+  signal video_clk : STD_LOGIC;
+begin
+
+IBUFGDS_i: component UNISIM.VCOMPONENTS.IBUFGDS
+   generic map (
+      DIFF_TERM => FALSE, 
+      IBUF_LOW_PWR => TRUE,
+      IOSTANDARD => "DEFAULT")
+   port map (
+      O => video_clk_i,
+      I => video_clk_p,
+      IB => video_clk_n
+   );
+
+BUFG_i: component UNISIM.VCOMPONENTS.BUFG
+   port map (
+      O => video_clk,
+      I => video_clk_i
+   );
+
+system_top_i: component system_top
+    port map (
+      DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
+      DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
+      DDR_cas_n => DDR_cas_n,
+      DDR_ck_n => DDR_ck_n,
+      DDR_ck_p => DDR_ck_p,
+      DDR_cke => DDR_cke,
+      DDR_cs_n => DDR_cs_n,
+      DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
+      DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
+      DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+      DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
+      DDR_odt => DDR_odt,
+      DDR_ras_n => DDR_ras_n,
+      DDR_reset_n => DDR_reset_n,
+      DDR_we_n => DDR_we_n,
+      FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+      FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+      FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
+      FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+      FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+      hdmi_clk => hdmi_clk,
+      hdmi_data(15 downto 0) => hdmi_data(15 downto 0),
+      hdmi_de => hdmi_de,
+      hdmi_hsync => hdmi_hsync,
+      hdmi_int => hdmi_int,
+      hdmi_vsync => hdmi_vsync,
+      video_clk => video_clk
+    );
+end STRUCTURE;
