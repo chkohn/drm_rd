047e94843c5ba12d927f010e871c5aca3dbbfd18
Chris Kohn <ckohn@xilinx.com>
2013-11-12 12:21:56 -0800

    sw: boot: update boot image

--- xmd/run_xmd.tcl
+++ xmd/run_xmd.tcl
@@ -1,6 +1,6 @@
 # tcl script to init arm and run sdk
 
-fpga -debugdevice devicenr 2 -f system.bit
+fpga -debugdevice devicenr 2 -f system_top_wrapper.bit
 connect arm hw
 source ps7_init.tcl
 ps7_init
